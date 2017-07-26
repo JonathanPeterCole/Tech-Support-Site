bd2dce02e4ae875b4b89a8e9b867dd8d1697b4c5
Jonathan Cole <JonathanPeterCole@gmail.com>
2017-07-26 13:14:41 +0100

    Added JS to fade in banner images on load

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+/**
+	* Main JS for my site, requires jQuery 3
+  * @author JonathanPeterCole@gmail.com
+**/
+
+// On Load Events
+$(window).on("load", function() {
+  // Fade in elements with the .fade-in-onload class
+  $(".fade-in").css("opacity", "100");
+});
