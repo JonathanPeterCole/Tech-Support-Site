0625fc6c6ea4ec41242608d3ef2df022f65f93f3
Jonathan Cole <JonathanPeterCole@gmail.com>
2017-07-28 15:50:43 +0100

    Added click event for the navbar buttons

--- static/js/main.js
+++ static/js/main.js
@@ -14,6 +14,17 @@ $(document).ready(function() {
     }
   });
 
+  // Navbar Buttons
+  $(".scroll-btn").click(function() {
+    event.preventDefault();
+    // Get the target
+    var target = $(this).attr("href");
+    // Get the offset
+    var offset = $(target).offset().top - 70;
+    // Scroll to the target
+    $('html,body').animate({scrollTop: offset}, 300);
+  });
+
   // On Load
   $(window).on("load", function() {
     // Fade in elements with the .fade-in-onload class
