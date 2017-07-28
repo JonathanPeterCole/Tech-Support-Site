3e7bbeba7f0d9f42280acf5ce867a4ac85e3a8b4
Jonathan Cole <JonathanPeterCole@gmail.com>
2017-07-28 15:00:13 +0100

    Added on-scroll effect to the header

--- static/js/main.js
+++ static/js/main.js
@@ -3,8 +3,20 @@
   * @author JonathanPeterCole@gmail.com
 **/
 
-// On Load Events
-$(window).on("load", function() {
-  // Fade in elements with the .fade-in-onload class
-  $(".fade-in").css("opacity", "100");
+// Document Ready Functions
+$(document).ready(function() {
+  // On Scroll
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 0) {
+      $("header").addClass("on-scroll");
+    } else {
+      $("header").removeClass('on-scroll');
+    }
+  });
+
+  // On Load
+  $(window).on("load", function() {
+    // Fade in elements with the .fade-in-onload class
+    $(".fade-in").css("opacity", "100");
+  });
 });
