bf0f31652b15ca1de426366e08dcf055444c48b3
Jonathan Cole <JonathanPeterCole@gmail.com>
2017-07-28 16:38:20 +0100

    Fixed the nav buttons on Firefox and split the header on-scroll event into a separate function

--- static/js/main.js
+++ static/js/main.js
@@ -3,38 +3,46 @@
   * @author JonathanPeterCole@gmail.com
 **/
 
-// Document Ready Functions
+// Event listeners
 $(document).ready(function() {
-  // On Scroll
+  // Set Header class incase user has already scrolled
+  setHeaderClass();
+
+  // On Scroll Event
   $(window).scroll(function() {
-    if ($(window).scrollTop() > 0) {
-      $("header").addClass("on-scroll");
-    } else {
-      $("header").removeClass('on-scroll');
-    }
+    setHeaderClass();
   });
 
-  // Navbar Buttons
-  $(".scroll-btn").click(function() {
+  // Navbar Button Event
+  $(".scroll-btn").click(function(event) {
     event.preventDefault();
-    // Get the target
+    // Get the target and offset
     var target = $(this).attr("href");
-    // Get the offset
     var offset = $(target).offset().top - 70;
     // Ensure the offset is within the page limits
     if (offset < 0) {
       offset = 0;
-    }
-    if (offset > $(document).height() - $(window).height()) {
+    } else if (offset > $(document).height() - $(window).height()) {
       offset = $(document).height() - $(window).height()
     }
     // Scroll to the target
     $('html,body').animate({scrollTop: offset}, 300);
   });
 
-  // On Load
+  // On Load Event
   $(window).on("load", function() {
     // Fade in elements with the .fade-in-onload class
     $(".fade-in").css("opacity", "100");
   });
 });
+
+// Functions
+function setHeaderClass() {
+  // The the user is not at the top of the page, add the on-scroll class,
+  // otherwise remove it
+  if ($(window).scrollTop() > 0) {
+    $("header").addClass("on-scroll");
+  } else {
+    $("header").removeClass('on-scroll');
+  }
+}
