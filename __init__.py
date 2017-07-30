14fefc90fc3a097a183f5cc717a5af2caf8f17bc
Jonathan Cole <JonathanPeterCole@gmail.com>
2017-07-30 18:32:11 +0100

    Disabled caching to avoid problems when updating the site

--- __init__.py
+++ __init__.py
@@ -10,5 +10,15 @@ mail = Mail(app)
 def index():
     return render_template('index.html.j2')
 
+@app.after_request
+def add_header(response):
+    # Disable cache
+    # Reference: https://stackoverflow.com/questions/34066804/disabling-caching-in-flask
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
 if __name__ == "__main__":
     app.run()
