5025165e4dfcfd9ae10a70239c47cf7d995d0780
Jonathan Cole <JonathanPeterCole@gmail.com>
2017-08-23 17:26:02 +0100

    Added basic fading between pages in page-manager

--- static/js/components/page-manager.js
+++ static/js/components/page-manager.js
@@ -6,16 +6,32 @@
 function pageManager(pageContainer) {
   // Set starting variables
   this.pageContainer = pageContainer;
+
   // Change the page
   this.setPage = function(targetPageID) {
+    // Create variables to hold the current and target divs
+    var currentPage;
+    var targetPage;
     // Loop through the pages in the container
-    $(this.pageContainer + " .page").each(function() {
+    $(this.pageContainer + ' .page').each(function() {
       // If the page ID matches, display it, otherwise hide it
-      if ($(this).attr('id') == targetPageID) {
-        $(this).addClass('display');
-      } else {
-        $(this).removeClass('display');
+      if($(this).attr('id') == targetPageID) {
+        targetPage = $(this);
+      } else if ($(this).hasClass('display')) {
+        currentPage = $(this);
       }
+    });
+    // Change the pages
+    switchPages(currentPage, targetPage);
+  }
+
+  // Switch page visibility
+  function switchPages(currentPage, targetPage) {
+    currentPage.fadeOut(200, function() {
+      currentPage.removeClass('display');
+      targetPage.fadeIn(200, function() {
+        targetPage.addClass('display');
+      });
     });
   }
 }
