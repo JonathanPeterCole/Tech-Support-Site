97595fc55261ce6efc8df7fe30a3096e61626fed
Jonathan Cole <JonathanPeterCole@gmail.com>
2017-08-31 16:02:54 +0100

    Updated the Regex to allow underscores

--- static/js/components/form-validation.js
+++ static/js/components/form-validation.js
@@ -5,12 +5,12 @@
 
 function formValidator() {
   // Prepare the Regex expressions
-  defaultExpression = new RegExp(/^[a-z0-9 ,.'@:()?!#+=£%&*\/\-\"]{0,50}$/i);
+  defaultExpression = new RegExp(/^[a-z0-9 ,.'@:()?!#+=£%&*_\/\-\"]{0,50}$/i);
   var expressionArray = {
     "name" : new RegExp(/^[a-z ,.'\-]{1,50}$/i),
     "email" : new RegExp(/^[a-z0-9!#$%&'*+\-/=?^_`{|}~]{1,50}@[a-z0-9\-]{1,50}\.[a-z0-9.\-]{1,50}[a-z0-9]$/i),
     "phone" : new RegExp(/^(0|\+44|44)[0-9]{10}$|^$/i),
-    "message" : new RegExp(/^(?=.*[a-z])[a-z0-9 ,.'@:()?!#+=£%&*\/\-\"\r\n]{1,5000}$/i)
+    "message" : new RegExp(/^(?=.*[a-z])[a-z0-9 ,.'@:()?!#+=£%&*_\/\-\"\r\n]{1,5000}$/i)
   };
 
   this.validateField = function(field) {
